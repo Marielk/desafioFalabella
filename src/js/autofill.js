6ccc02139ecec0fff7d34ef9449ade3950916a60
Mariel <marielk.mya@gmail.com>
2018-10-25 16:19:34 -0300

    arreglando fallos de filtro productos

--- src/js/autofill.js
+++ src/js/autofill.js
@@ -1,24 +1,62 @@
 const firestore = firebase.firestore();
 let namesAndStore = [];
 let fullData = [];
+let newDataML = [];
 const showList = document.getElementById('placeList');
 const showProductPlace = document.getElementById('productResultPlace');
 const showML = document.getElementById('productResultPlaceML');
 
-const getData = () => {
-  const settings = {/* your settings... */ timestampsInSnapshots: true };
-  firestore.settings(settings);
-  firestore.collection("products")
-    .get()
-    .then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        formObject(doc);
-        getFullData(doc);
-      })
-      let search = getInput();
+// const getData = () => {
+//   const settings = {/* your settings... */ timestampsInSnapshots: true };
+//   firestore.settings(settings);
+//   firestore.collection("products")
+//     .get()
+//     .then(function (querySnapshot) {
+//       querySnapshot.forEach(function (doc) {
+//         formObject(doc);
+//         getFullData(doc);
+//       })
+//       let search = getInput();
+//       if (search !== null && search !== undefined && search.length > 1) {
+
+//         let encontrados = namesAndStore.forEach(element => {
+//           // console.log(element.name);
+//           // console.log(search);
+//           let nombres = element.name.toLowerCase();
+//           // let result = [];
+//           if (nombres.search(search) != -1) {
+
+//             // console.log('tiene' + element.name);
+//             let id = element.name.replace(/ /g, "");
+//             showList.innerHTML += `
+//             <li class="list-group-item">${element.name}<button class="float-right btn verde" id=${id} onclick="showProduct(this.id)">Comparar</button></li>
+//            `
+//             // result.push(nombres)
+//           } else { // console.log('nothing');
+//           }
+//         })
+//       } else {
+//         console.log('input vacio');
+//       }
+//     })
+//     .catch(function (error) {
+//       console.log("Error getting documents: ", error);
+//     });
+
+    const getData = () => {
+      dataExtractor('../../products.json').then((result) => {
+        // console.log('promesa ejecutada' + JSON.stringify(result));
+        result.forEach(element => {
+          // console.log(element);
+          formObject(element);
+          getFullData(element);
+        })  
+        let search = getInput();
       if (search !== null && search !== undefined && search.length > 1) {
 
         let encontrados = namesAndStore.forEach(element => {
+          // console.log(namesAndStore);
+          
           // console.log(element.name);
           // console.log(search);
           let nombres = element.name.toLowerCase();
@@ -29,34 +67,26 @@ const getData = () => {
             let id = element.name.replace(/ /g, "");
             showList.innerHTML += `
             <li class="list-group-item">${element.name}<button class="float-right btn verde" id=${id} onclick="showProduct(this.id)">Comparar</button></li>
-           `
-            // result.push(nombres)
-          } else { // console.log('nothing');
+           `;
           }
-        })
-      } else {
-        console.log('input vacio');
-      }
+          })
+        }  
     })
-    .catch(function (error) {
-      console.log("Error getting documents: ", error);
-    });
-
-
+  }
 
   const formObject = (doc) => {
     namesAndStore.push({
-      "name": doc.data().producto,
-      "seller": doc.data().tienda
+      "name": doc.Producto,
+      "seller": doc.Tienda
     });
   };
   const getFullData = (doc) => {
     // console.log(doc.data().producto);
     fullData.push({
-      "nombre": doc.data().producto,
-      "url": doc.data().image,
-      "seller": doc.data().tienda,
-      "precioFalabella": doc.data().valorOrigen
+      "nombre": doc.Producto,
+      "url": doc.Thumbnail,
+      "seller": doc.Tienda,
+      "precioFalabella": doc.ValorUnidad
     })
     // console.log(fullData);
 
@@ -69,12 +99,12 @@ const getData = () => {
       return search;
     };
   }
-}
+
 
 const showProduct = (id) => {
 
   //console.log(id);
-  //console.log(fullData[0].nombre);
+  // console.log(fullData);
   let match = fullData.find((element) => {
     if (element.nombre.replace(/ /g, "") === id) {
       return element;
@@ -97,17 +127,44 @@ const showProduct = (id) => {
 };
 
 const showProductML = (id, match) => {
-  console.log(match);
-  
-  showML.innerHTML = `
-    <div class="col-md-1 col-4">
-    <img src="./img/mercado libre.png" class=" rounded-circle" height="40px">
-  </div>
-  <div class="col-md-2 col-4">
-    <img class=" align-middle" src="${match.url}">
-  </div>
-  <div class="col-md-6">${match.nombre}/${match.seller}</div>
+  const settings = {/* your settings... */ timestampsInSnapshots: true };
+  firestore.settings(settings);
+  firestore.collection("products")
+  .get()
+  .then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      newDataML.push({
+        "nombre": doc.data().producto,
+        "url": doc.data().image,
+        "seller": doc.data().tienda,
+        "precioFalabella": doc.data().valorOrigen,
+        "precioML": doc.data().precioML,
+        "sellerNick": doc.data().seller
+      });
+    })
+    
+  }).catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+    // console.log(newDataML);
+      let match2 = newDataML.find((element) => {
+        if (element.nombre.replace(/ /g, "") === id) {
+          console.log(element);
+          return element;
+        }  
+        
+      showML.innerHTML = `
+        <div class="col-md-1 col-4">
+        <img src="./img/mercado libre.png" class=" rounded-circle" height="40px">
+      </div>
+      <div class="col-md-2 col-4">
+        <img class=" align-middle" src="${match.url}">
+      </div>
+      <div class="col-md-6">${match.nombre}/${match.seller}</div>
 
-  <div class="col-md-3">$399990</div>
-  `
-};
+      <div class="col-md-3">$${match2.precioML}</div>
+      `
+        })
+
+  
+}
